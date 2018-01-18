d5428fb1061a961c4e27784a7e3b66c23a5fc4e1
Katarina Brilej <katarina.brilej@gmail.com>
2018-01-18 07:32:15 +0100

    odstranjen print

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -6,7 +6,6 @@ library(dplyr)
 umorjeni.urejeno <- umorjeni[order(umorjeni$umorjeni, decreasing=TRUE), ] %>% .[1:10, ]
 graf1 <- ggplot(umorjeni.urejeno) + aes(x = Drzava, y = umorjeni, fill = Drzava) + geom_col() + 
   theme(axis.text.x = element_blank())
-print(graf1)
 
 #gibanje kriminalitete v ljubljani v obdobju 2006-2015
 
@@ -14,5 +13,11 @@ Ljubljana <- brezposelnost_in_obsojeni[brezposelnost_in_obsojeni$obcina == "Ljub
 graf2 <- ggplot(Ljubljana) + aes(x = leto, y = stopnja, colour = meritev) + geom_line()
 
 
-
+# Uvozimo zemljevid.
+zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
+                             "OB/OB", encoding = "Windows-1250")
+levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
+{ gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
+zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(obcine$obcina))
+zemljevid <- pretvori.zemljevid(zemljevid)
 
