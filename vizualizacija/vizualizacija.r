784a498ef17ef6933c63473bf98a2f545f3773d3
Katarina Brilej <katarina.brilej@gmail.com>
2018-02-11 21:36:21 +0100

    dodan zemljevid in grafi

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -3,23 +3,64 @@ library(ggplot2)
 library(dplyr)
 
 #10 držav z največjih številom umorjenih na 100000 prebivalcev
-umorjeni.urejeno <- umorjeni[order(umorjeni$umorjeni, decreasing=TRUE), ] %>% .[1:10, ]
-graf1 <- ggplot(umorjeni.urejeno) + aes(x = Drzava, y = umorjeni, fill = Drzava) + geom_col() + 
-  theme(axis.text.x = element_blank())
+graf.umorjeni <- ggplot(umorjeni2[order(umorjeni2$umorjeni, decreasing=TRUE), ] %>% .[1:10, ]) + 
+  aes(x = Drzava, y = umorjeni, fill = Drzava) + geom_col() + 
+  theme(axis.text.x = element_blank()) +
+  labs(title = "10 drzav z najvisjo stopnjo umorjenih") +
+  ylab("Stopnja")
+
+print(graf.umorjeni)
+
+#10 držav z najmanjšim številom umorjenih na 100000 prebivalcev
+graf.umorjeni2 <- ggplot(umorjeni2[order(umorjeni2$umorjeni, decreasing=FALSE), ] %>% .[1:10, ]) + 
+  aes(x = Drzava, y = umorjeni, fill = Drzava) + geom_col() + 
+  theme(axis.text.x = element_blank()) +
+  labs(title = "10 drzav z najnizjo stopnjo umorjenih") +
+  ylab("Stopnja")
+
+print(graf.umorjeni2)
+
+#10 držav z največjih številom zaprtih na 100000 prebivalcev
+graf.zaprti <- ggplot(zaprti2[order(zaprti2$zaprti, decreasing=TRUE), ] %>% .[1:10, ]) + 
+  aes(x = Drzava, y = zaprti, fill = Drzava) + geom_col() + 
+  theme(axis.text.x = element_blank()) +
+  labs(title = "10 drzav z najvisjo stopnjo zaprtih") +
+  ylab("Stopnja")
+
+print(graf.zaprti)
+
+#10 držav z najmanjšim številom zaprtih na 100000 prebivalcev
+graf.zaprti2 <- ggplot(zaprti2[order(zaprti2$zaprti, decreasing=FALSE), ] %>% .[1:10, ]) + 
+  aes(x = Drzava, y = zaprti, fill = Drzava) + geom_col() + 
+  theme(axis.text.x = element_blank()) +
+  labs(title = "10 drzav z najnizjo stopnjo zaprtih") +
+  ylab("Stopnja")
+
+print(graf.zaprti2)
+
+graf.umorjeni.zaprti <- ggplot(data = zaprti_in_umorjeni)+
+  geom_point(aes(x = umorjeni, y = zaprti), stat = 'identity')+
+  #geom_text(aes(x = umorjeni, y = zaprti, label=Drzava),hjust=0, vjust=0)+
+  labs(title ="Primerjava stopnje zaprtih in stopnje umorjenih")+
+  ylab("Stopnja zaprtih") + xlab("Stopnja umorjenih")
+
+print(graf.umorjeni.zaprti)
 
 #gibanje kriminalitete v ljubljani v obdobju 2006-2015
 
 Ljubljana <- brezposelnost_in_obsojeni[brezposelnost_in_obsojeni$obcina == "Ljubljana",]
 graf2 <- ggplot(Ljubljana) + aes(x = leto, y = stopnja, colour = meritev) + geom_line()
+print(graf2)
+
+#gibanje stevila brezposelnih in obsojenih v Sloveniji v obdobju 2006-2015
+graf.gibanje <- ggplot(brezposelnost_in_obsojeni)
 
 
 # Uvozimo zemljevid.
 
 zemljevid <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
                              "ne_50m_admin_0_countries", 
-                             encoding = "UTF-8") %>%
-  pretvori.zemljevid()
-
+                             encoding = "UTF-8") %>% pretvori.zemljevid()
 
 colnames(zemljevid)[11] <- 'drzava'
 
@@ -32,22 +73,32 @@ zemljevid$drzava[zemljevid$drzava == "United Republic of Tanzania"] <- "Tanzania
 zemljevid$drzava[zemljevid$drzava == "United States of America"] <- "United States"
 
 
-umorjeni$Drzava[umorjeni$Drzava == "Micronesia, Fed. Sts."] <- "Federated States of Micronesia"
 
 
 zemljevid.umorjeni <- ggplot() +
-  geom_polygon(data = umorjeni %>% right_join(zemljevid, by = c("Drzava" = "drzava")),
-               aes(x = long, y = lat, group = group, fill = umorjeni), color = "black")+
+  geom_polygon(data = umorjeni2 %>% right_join(zemljevid, by = c("Drzava" = "drzava")),
+               aes(x = long, y = lat, group = group, fill = umorjeni), alpha = 0.8, color = "black")+
+  scale_fill_gradient2(low = "yellow", mid = "red",
+                       high = "brown", midpoint = 75) + 
   xlab("") + ylab("") + ggtitle("Stopnja umorjenih po svetu")
 
-#print(zemljevid.umorjeni)
-  
+print(zemljevid.umorjeni)
+
   
 zemljevid.zaprti <- ggplot() +
-    geom_polygon(data = zaprti %>% right_join(zemljevid, by = c("Drzava" = "drzava")),
+    geom_polygon(data = zaprti2 %>% right_join(zemljevid, by = c("Drzava" = "drzava")),
                  aes(x = long, y = lat, group = group, fill = zaprti), color = "black")+
     xlab("") + ylab("") + ggtitle("Stopnja zaprtih po svetu")
   
-#print(zemljevid.zaprti)
+print(zemljevid.zaprti)
+
+obcine <- uvozi.zemljevid("http://www.stat.si/doc/Geo/Obcine_LAU2.zip", 
+                             "Obcine",encoding = "Windows-1250") %>% pretvori.zemljevid()
+zemljevid.obsojeni <- ggplot() +
+  geom_polygon(data = obsojeni_po_obcinah2 %>% filter(leto == "2016") %>% right_join(obcine, by = c("obcina" = "IME")),
+               aes(x = long, y = lat, group = group, fill = obsojeni), color = "black")+
+  xlab("") + ylab("") + ggtitle("Stopnja obsojenih po slovenskih občinah")
+
+
   
 
