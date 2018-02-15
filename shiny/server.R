09422f5c4afc376719679a5af6e92f797d5ac9c3
Katarina Brilej <katarina.brilej@gmail.com>
2018-02-15 11:11:24 +0100

    začet shiny

--- shiny/server.R
+++ shiny/server.R
@@ -1,24 +1,11 @@
 library(shiny)
 
-shinyServer(function(input, output) {
-  output$druzine <- DT::renderDataTable({
-    dcast(druzine, obcina ~ velikost.druzine, value.var = "stevilo.druzin") %>%
-      rename(`Občina` = obcina)
-  })
+function(input, output) {
   
-  output$pokrajine <- renderUI(
-    selectInput("pokrajina", label="Izberi pokrajino",
-                choices=c("Vse", levels(obcine$pokrajina)))
-  )
-  output$naselja <- renderPlot({
-    main <- "Pogostost števila naselij"
-    if (!is.null(input$pokrajina) && input$pokrajina %in% levels(obcine$pokrajina)) {
-      t <- obcine %>% filter(pokrajina == input$pokrajina)
-      main <- paste(main, "v regiji", input$pokrajina)
-    } else {
-      t <- obcine
-    }
-    ggplot(t, aes(x = naselja)) + geom_histogram() +
-      ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
+  output$grafi1 <- renderPlot({
+    graf2 <- ggplot(brezposelnost_in_obsojeni %>% filter(obcina == input$obcina1)) + 
+      aes(x = leto, y = stopnja, colour = meritev) + geom_line()+
+      labs(title = "gibanje kriminalitete in brezposelnosti v Ljubljani")
+    print(graf2)
   })
-})
+}
